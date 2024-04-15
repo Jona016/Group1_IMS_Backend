748240b4c1482971986a53296622afdb54f69541
Jonalyn Llanes <jllanes2@my.centennialcollege.ca>
2024-04-14 20:40:10 -0400

    Controller, Helper Modified

--- helpers/auth.js
+++ helpers/auth.js
@@ -4,9 +4,8 @@ import config from '../config/config.js'
 const getTokenUserId = (token) => {
     try {
       const decoded = jwt.verify(token, config.jwtSecret);
-      return decoded.userId; // Assuming the user ID is stored in the token's payload under the key '_id'
+      return decoded.userId; 
     } catch (error) {
-      // Handle invalid token or other errors
       return null;
     }
   }
